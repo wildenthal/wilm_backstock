dcd413e43d3b663e0faee059a651eb0bcb6963b8
wildenthal <8503469+wildenthal@users.noreply.github.com>
2021-10-12 11:24:00 +0200

    Initial deployment
    Reads/writes to MongoDB Atlas, lists backstocked items, cross-checks with inbounded items according to excel generated by bulk-inbound page.

---
+++ streamlit_app.py
@@ -0,0 +1,89 @@
+import streamlit as st
+import datetime
+from pymongo import MongoClient
+import pandas as pd
+
+
+# LOADS WAREHOUSE
+# Initialize connection.
+cluster = "mongodb+srv://Tomai:Hz3ry40lNFPeio0P@warehouse.konbq.mongodb.net/Warehouses?retryWrites=True&w=majority"
+client = MongoClient(cluster)#(**st.secrets["mongo"]) cannot make secrets.toml work!
+db = client.Warehouses
+stocklist = db.wilm
+
+#Website formatting
+st.title("Wilmersdorf fridge stocks")
+task1 = 'Cross check'
+task2 = 'View backstocked refrigerated items'
+task3 = 'Add/remove items'
+task = st.sidebar.radio('Select operation',[task1, task2, task3])
+
+#Cross check new items with backstock ones
+if task == task1: 
+    st.sidebar.markdown('### Upload excel file from new delivery')
+    newitemsraw = st.sidebar.file_uploader("File will be cross-checked with backstock", type=["xls","xlsx"])
+    #Once uploaded:
+    if newitemsraw is not None:
+        newitems = pd.read_excel(newitemsraw)['SKU'] #loads only SKU data
+        matches = list(stocklist.find({'SKU': { '$in': list(newitems)}})) 
+        if len(matches) == 0:
+            st.info('All items must be taken to front.')
+        else:
+            st.info('The following items are present in backstock.') 
+            st.markdown('---')
+            number = 1
+            for item in matches:
+                st.markdown('{}. {}'.format(number, item["name"]))
+                number += 1 #matches is a cursor
+            st.markdown('---')
+            st.info('Please put new items in fridge and bring old items to front.')
+
+#View stocked items
+if task == task2: 
+    liststock = list(stocklist.find())
+    for stockitem in liststock:
+        if len(stockitem["batches"])>0:
+            st.markdown("---\n{} ({})  -  **{}**\n -\n EAN: {}".format(stockitem["name"],stockitem["SKU"],stockitem["shelf"],stockitem["EAN"]))
+            for expdate in stockitem["batches"]:
+                st.info(("{} items expire on {}\n".format(stockitem["batches"][expdate],expdate)))
+    
+#Scan items and modify their stock
+if task == task3: 
+    st.sidebar.markdown('### Scan item and press Enter')
+    EAN = int('0'+st.sidebar.text_input('EAN (Select to scan)'))
+    #Once scanned:
+    if EAN != 0:
+        #Find and print item
+        stock_item = stocklist.find_one({'EAN': EAN})
+        st.markdown('---')
+        st.subheader("{} ({})  -  **{}**\n".format(stock_item["name"],stock_item["SKU"],stock_item["shelf"]))
+        st.text('')
+        #Update form:
+        form = st.form(key='updatestock')
+        expdate = form.date_input('Enter expiration date')
+        amount = form.number_input('Enter inventory change (positive or negative)',step=1,format='%i')
+        submit = form.form_submit_button(label='Submit')
+        
+        #Save new warehouse
+        if submit: #only excecutes on rerun
+            expdate = expdate.strftime("%Y.%m.%d")
+            try:
+                stock_item['batches'][expdate] += amount
+            except:
+                stock_item['batches'][expdate] = amount
+            if stock_item['batches'][expdate] <= 0:
+                del stock_item['batches'][expdate]
+            stocklist.update_one({'EAN':EAN},{'$set':{'batches':stock_item['batches']}})
+            flag = False
+            for expdate in stock_item["batches"]:
+                st.info("{} items expire on {}\n".format(stock_item["batches"][expdate],expdate))
+                flag = True
+            if flag == False:
+                st.info("No items in backstock")
+        else: #if no update is sent, prints stock anyway
+            flag = False
+            for expdate in stock_item["batches"]:
+                st.info("{} items expire on {}\n".format(stock_item["batches"][expdate],expdate))
+                flag = True
+            if flag == False:
+                st.info("No items in backstock")
