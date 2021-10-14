fe1d0f586227d5de72b558fb6fd2536303b740bf
wildenthal <8503469+wildenthal@users.noreply.github.com>
2021-10-14 12:15:40 +0200

    Starts adding advanced options
    Allows missing EAN loading

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,10 +16,27 @@ st.title("Wilmersdorf fridge stocks")
 task1 = 'Cross check'
 task2 = 'View backstocked refrigerated items'
 task3 = 'Add/remove items'
-task = st.sidebar.radio('Select operation',[task1, task2, task3])
+task = st.sidebar.radio('Normal operations',[task1, task2, task3])
+
+#Rerun counter
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+print('This is run {}.'.format(st.session_state.count))
+st.session_state.count += 1
+
+#Button to toggle advanced options
+advanced = st.sidebar.button('Toggle advanced options')
+#Session state to store whether advanced options are enabled
+if 'advenabled' not in st.session_state:
+	st.session_state.advenabled = False
+#Resets advanced options when toggled twice and reruns to clear
+if advanced == True and st.session_state.advenabled == True:
+    st.session_state.advenabled = False
+    st.experimental_rerun()
+
 
 #Cross check new items with backstock ones
-if task == task1: 
+if task == task1 and st.session_state.advenabled == False: 
     st.sidebar.markdown('### Upload excel file from new delivery')
     newitemsraw = st.sidebar.file_uploader("File will be cross-checked with backstock", type=["xls","xlsx"])
     #Once uploaded:
@@ -39,7 +56,7 @@ if task == task1:
             st.info('Please put new items in fridge and bring old items to front.')
 
 #View stocked items
-if task == task2: 
+if task == task2 and st.session_state.advenabled == False: 
     liststock = list(stocklist.find())
     for stockitem in liststock:
         if len(stockitem["batches"])>0:
@@ -48,7 +65,7 @@ if task == task2:
                 st.info(("{} items expire on {}\n".format(stockitem["batches"][expdate],expdate)))
     
 #Scan items and modify their stock
-if task == task3: 
+if task == task3 and st.session_state.advenabled == False: 
     st.sidebar.markdown('### Scan item and press Enter')
     EAN = int('0'+st.sidebar.text_input('EAN (Select to scan)'))
     #Once scanned:
@@ -87,3 +104,28 @@ if task == task3:
                 flag = True
             if flag == False:
                 st.info("No items in backstock")
+
+if advanced == True:
+    st.session_state.advenabled = True
+    st.experimental_rerun()
+
+if st.session_state.advenabled == True:
+    task4 = 'Correct EAN/SKU/name/shelf'
+    task5 = 'View all refrigerated items'
+    task = st.sidebar.radio('Disable advanced options for normal operation',[task4,task5])
+    if task == task4:
+        option = st.sidebar.selectbox('Select field to correct',('Enter missing EANS','Add missing item','Edit existing item'))
+        if option == 'Enter missing EANS':
+            st.button('Click to refresh')
+            flag = False
+            for shelfnumber in range(9,15):
+                st.markdown('### For Fridge {}:'.format(shelfnumber))
+                stockshelf = stocklist.find({'EAN':0,'shelf':shelfnumber})
+                for stockitem in stockshelf:
+                    EAN = int('0'+st.text_input('Please enter EAN for {} on shelf {} with SKU {}'.format(stockitem['name'],stockitem['letter'],stockitem['SKU'])))
+                    if EAN !=0:
+                        stocklist.update_one({"_id" : stockitem["_id"] }, {"$set" : {"EAN" :EAN}})
+                        st.markdown('Success!')
+                    flag=True
+            if flag == False:
+                st.markdown('### All items have EANS!')
